c207c767f13ce3675b65422bbfe618ea47af8d05
Syed Sohail <74762045+SyedSohail786@users.noreply.github.com>
2025-05-20 23:44:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,17 +20,17 @@ Sentry.setupExpressErrorHandler(app);
 
 //FOR LOCALHOST
 
-mongoose.connect(process.env.DATABASE_URI + process.env.DATABASE_NAME)
-.then(() => console.log(`${process.env.DATABASE_NAME} Database Connected!`));
-app.listen(process.env.PORTNUMBER, ()=>{
-     console.log(`Server Started on Port ${process.env.PORTNUMBER}`);
-     
-})
-
-//FOR MONGODB ATLAS
 // mongoose.connect(process.env.DATABASE_URI + process.env.DATABASE_NAME)
 // .then(() => console.log(`${process.env.DATABASE_NAME} Database Connected!`));
 // app.listen(process.env.PORTNUMBER, ()=>{
 //      console.log(`Server Started on Port ${process.env.PORTNUMBER}`);
      
 // })
+
+FOR MONGODB ATLAS
+mongoose.connect(process.env.DATABASE_URI + process.env.DATABASE_NAME)
+.then(() => console.log(`${process.env.DATABASE_NAME} Database Connected!`));
+app.listen(process.env.PORTNUMBER, ()=>{
+     console.log(`Server Started on Port ${process.env.PORTNUMBER}`);
+     
+})
